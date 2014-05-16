07dfdbb7b1d5760571d2f51ee372cf7c3658f605
BlueVajra <cory.leistikow@gmail.com>
2014-05-16 12:20:25 -0600

    [#69502474] User can login

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
 feature "Signup" do
-  scenario "User can register" do
+  before do
     visit '/'
-    click_link 'Please Register'
+    click_link 'Sign up'
     fill_in 'user_email', with: 'bob@bob.com'
     fill_in 'user_password', with: 'abcd1234'
     fill_in 'user_password_confirmation', with: 'abcd1234'
     click_button 'Sign up'
     expect(page).to have_content 'Welcome! You have signed up successfully.'
+
+  end
+  scenario "User can register, sign out and sign in" do
+    click_link 'Sign out'
+    expect(page).to have_content 'Signed out successfully.'
+    click_link 'Sign in'
+    fill_in 'user_email', with: 'bob@bob.com'
+    fill_in 'user_password', with: 'abcd1234'
+    click_button 'Sign in'
+    expect(page).to have_content 'Signed in successfully.'
   end
 end
